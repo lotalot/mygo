5e4e14f67460b17dc0d5d6e8340bc50772eb2440
TKE\lenovo <13634327650@163.com>
2024-04-27 15:42:38 +0800

    Changes

--- src/study/zgorm.go
+++ src/study/zgorm.go
@@ -7,9 +7,10 @@ package main
 //gorm进行安全操作可以进行增加等，不会进行删除或改变
 import (
 	"fmt"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm" //此包调用的是gorm v2版本，与之前版本有较大区别
 )
 
 type Test struct { //可以在里面直接内嵌别的结构体
@@ -21,11 +22,12 @@ type Test struct { //可以在里面直接内嵌别的结构体
 
 func maino() {
 	//连接数据库
-	db, err := gorm.Open("mysql", "root:11575@(127.0.0.1:3306)/myku?charset=utf8mb4&parseTime=True")
+	db, err := gorm.Open(mysql.Open("root:11575@tcp(127.0.0.1:3306)/myku?charset=utf8mb4"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	sqlDB, err := db.DB() //v2版本中关闭数据库的操作
+	defer sqlDB.Close()
 
 	db.AutoMigrate(&Test{}) //如果表不存在则自动创建
 	//创建
@@ -47,4 +49,24 @@ func maino() {
 	//链式操作，Find()等函数为立即执行函数，会直接翻译成sql语句，而where()等并不会，
 	//可以将多个where查询付给一条语句然后再进行执行翻译为sql语句
 
+	//——————————————viper———————————————
+	//viper是一个配置解决的工具包
+	viper.SetConfigName("config") // 设置配置文件的名称，不包含扩展名
+	viper.AddConfigPath("bag")    // 添加配置文件的搜索路径
+	errs := viper.ReadInConfig()  // 读取配置文件
+	if errs != nil {
+		// 处理错误
+	}
+	viper.SetDefault("ContentDir", "content")                       //建立默认值
+	dbs, err := gorm.Open(mysql.Open(viper.GetString("mysql.dns"))) // 获取字符串类型的配置值
+	dbs.AutoMigrate(&Test{})
+
+	viper.Set("name", "张三")                 //为name设置一个username的别名
+	viper.RegisterAlias("username", "name") //通过username可以读取到name的值
+
+	fmt.Println(viper.Get("username")) // 设置监听回调
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("检测到配置文件已更改:", e.Name)
+	})
+	viper.WatchConfig() // 开启监听
 }
